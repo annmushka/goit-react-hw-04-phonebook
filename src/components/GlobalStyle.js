3e75be886b8312c0643696a413a782cc6b502cba
Ann <ann.mushka@gmail.com>
2023-02-22 00:37:57 +0200

    add hooks

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+}
+body {
+    margin: 0;
+    padding:0;
+}
+ul {
+    padding-left: 0;
+}
+li {
+    margin-bottom: 10px;
+}
+h1, h2, p {
+margin: 0;
+    padding:0;
+}
+h1 {
+    font-size: 28px;
+    margin-bottom: 20px;
+}
+h2 {
+    font-size: 24px;
+    margin-bottom: 15px;
+}
+p {
+    font-size: 20px;
+}
+`;
